454e6ae3217fc77b4cefe3ff7e50067589b6222f
ano.emangi <ano.emangi@minutoseguros.com.br>
2022-04-26 11:12:19 -0300

    Add context class and install EntityFrameworkCore

---
+++ multiple-Context/MultipleDbContext.Api/Data/BaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MultipleDbContext.Api.Data
+{
+    public class BaseContext : DbContext
+    {
+        public BaseContext(DbContextOptions options):base(options)
+        {
+        }
+    }
+}
---
+++ multiple-Context/MultipleDbContext.Api/Data/DbTwoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MultipleDbContext.Api.Data
+{
+    public class DbTwoContext : BaseContext
+    {
+        public DbTwoContext(DbContextOptions<DbTwoContext> options) : base(options)
+        {
+        }
+    }
+}
